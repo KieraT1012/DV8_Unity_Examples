07d5403cc43661d71e6b2a1ff76e53d065fa6d05
JamesDV8Games <j.brandon@dv8learn.org.uk>
2022-01-26 14:35:08 +0000

    Added my Material to project

---
+++ Assets/_Test Folder/Test_James/JamesMaterial/MyMaterialDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyMaterialDisplay : MonoBehaviour
+{
+
+    Transform sphere;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sphere = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sphere.transform.Rotate(0, 20 * Time.deltaTime, 0);
+    }
+}
